188ebe31f34cfe121347d26df12b4b84d6eed75a
GUILLOU <antoine.guillou@cgi.com>
2022-09-23 23:14:37 +0200

    import fichiers test2

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,6 @@ import '@testing-library/jest-dom/extend-expect';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);;
   expect(linkElement).toBeInTheDocument();
 });
