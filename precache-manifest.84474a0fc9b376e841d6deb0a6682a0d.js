67f7fcdc8fce1561c428e00692910697ea6b6fb6
Antoine1989 <99190941+Antoine1989@users.noreply.github.com>
2022-09-28 17:03:52 +0000

    Build: (53a1688) Create publish.yml

---
+++ precache-manifest.84474a0fc9b376e841d6deb0a6682a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9abd74693b139d19543c",
+    "url": "./static/css/main.9b6efa2f.chunk.css"
+  },
+  {
+    "revision": "9abd74693b139d19543c",
+    "url": "./static/js/main.9abd7469.chunk.js"
+  },
+  {
+    "revision": "cf2dc106f32c680f6556",
+    "url": "./static/css/1.349906f4.chunk.css"
+  },
+  {
+    "revision": "cf2dc106f32c680f6556",
+    "url": "./static/js/1.cf2dc106.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6838bb62427b352e1867a6b9e35e93b8",
+    "url": "./index.html"
+  }
+];
